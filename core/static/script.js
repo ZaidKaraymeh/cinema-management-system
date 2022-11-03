274cb44fb7c5a579c05bdd22f3be908304f71700
Zaid <karaymehzaid@gmail.com>
2022-11-03 22:58:31 +0300

    Push Elements Below Nav

--- core/static/script.js
+++ core/static/script.js
@@ -1,9 +1,11 @@
+
+
 document.documentElement.style.setProperty(
   "--nav-height",
-  document.getElementById("navbar_cstm").offsetHeight
+  document.getElementById("navbar_cstm").offsetHeight + "px"
 );
 
 
 
-const main = document.getElementsByClassName("body")
-main.style.marginTop = document.getElementById("navbar_cstm").offsetHeight + 10;
+/* const main = document.getElementsByClassName("body")
+main.style.marginTop = document.getElementById("navbar_cstm").offsetHeight + 10;  */
