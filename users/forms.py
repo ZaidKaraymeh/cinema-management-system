283a15747f87613125ebb57ee2f77150c2716f7a
Zaid <karaymehzaid@gmail.com>
2022-11-04 15:59:17 +0300

    Login, Register Styling

--- users/forms.py
+++ users/forms.py
@@ -3,13 +3,22 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 from .models import User
+from django.contrib.auth.forms import AuthenticationForm
 
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['style'] = 'height:50px;'
+        self.fields['password'].widget.attrs['style'] = 'height:50px;'
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password'].widget.attrs['class'] = 'form-control'
 
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
     birth = forms.DateField(
-        label='', widget=forms.SelectDateWidget(years=range(2012, 1950, -1)))
+        label='Birth Date', widget=forms.SelectDateWidget(years=range(2012, 1950, -1)))
     class Meta:
         # Interacts with User model
         model = User
@@ -23,6 +32,23 @@ class RegisterForm(UserCreationForm):
 
         for fieldname in ['password1', 'password2']:
             self.fields[fieldname].help_text = None
+
+        self.fields['first_name'].widget.attrs['style'] = 'width:100%; height:50px;'
+        self.fields['last_name'].widget.attrs['style'] = 'width:100%; height:50px;'
+        self.fields['email'].widget.attrs['style'] = 'width:100%; height:50px;'
+        self.fields['password1'].widget.attrs['style'] = 'width:100%; height:50px;'
+        self.fields['password2'].widget.attrs['style'] = 'width:100%; height:50px;'
+        self.fields['user_type'].widget.attrs['style'] = 'width:100%; height:50px;'
+        self.fields['birth'].widget.attrs['style'] = 'height:50px;'
+        self.fields['phone_number'].widget.attrs['style'] = 'width:100%; height:50px;'
+        self.fields['first_name'].widget.attrs['class'] = 'form-control'
+        self.fields['last_name'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['user_type'].widget.attrs['class'] = 'form-select'
+        self.fields['birth'].widget.attrs['class'] = 'form-select'
+        self.fields['phone_number'].widget.attrs['class'] = 'form-control'
 
 """
 class UserUpdateForm(forms.ModelForm):
