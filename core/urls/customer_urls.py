654733cd781eb3a103789093a16540dee8848717
mohdShahwan <mshahwan002@gmail.com>
2023-01-01 01:14:57 +0300

    Tickets

--- core/urls/customer_urls.py
+++ core/urls/customer_urls.py
@@ -4,5 +4,7 @@ from core.views import customer_views
 
 urlpatterns = [
     path('book/<str:schedule_id>', customer_views.customer_movie_booking, name="customer_movie_booking"),
-    path('ticket/<str:schedule_id>/<str:user_id>', customer_views.book_ticket_json, name="book_ticket_json"),
+    path('payment/<str:schedule_id>/<str:user_id>', customer_views.book_ticket_json, name="book_ticket_json"),
+    #path('tickets/<str:transaction_id>', customer_views.tickets, name="tickets"),
+    path('tickets', customer_views.tickets, name="tickets"),
 ]
